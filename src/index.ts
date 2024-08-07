321f8761423339ef3e60436d325b417754f89d45
Stuart Bowman <sbowman@satellite.earth>
2024-08-06 17:28:19 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ global.WebSocket = WebSocket;
 useWebSocketImplementation(WebSocket);
 
 const server = createServer();
-// const wss = new WebSocketServer({ server });
 
 // Create websocket server
 const wss = new WebSocketServer({ server });
@@ -20,25 +19,7 @@ wss.on('headers', (headers: any) => {
 	headers.push('Access-Control-Allow-Origin: *');
 });
 
-// TODO manager
-
-// DEV STUFF PLEASE IGNORE
-// const Manager = {
-// 	apps: {
-// 		sbowman: new App('/Users/sbowman/Desktop/demo/sbowman'),
-// 	},
-// };
-// Manager.apps.sbowman.control.attachToServer(wss);
-// const sbowmanPubkey = 'ff27d01cb1e56fb58580306c7ba76bb037bf211c5b573c56e4e70ca858755af0';
-// Manager.apps.sbowman.config.setField('owner', sbowmanPubkey);
-// // Setup handlers for new connections
-// wss.on('connection', (ws, req) => {
-// 	if (req.url === '/') return Manager.apps.sbowman.relay.handleConnection(ws, req);
-// 	Manager.apps.sbowman.relay.handleConnection(ws, req);
-// });
-// Manager.apps.sbowman.start();
-
 // Listen for http connections
 server.listen(PORT, () => {
-	console.log('servecdr running on port ' + PORT);
+	console.log('server running on port ' + PORT);
 });
